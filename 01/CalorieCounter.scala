4c6024c396b73bfe6049293957e1ffa2b99d8061
Alberto G. Jácome <contact@agjacome.dev>
2022-12-02 11:59:29 +0100

    day1 avoid extra sum step

--- 01/CalorieCounter.scala
+++ 01/CalorieCounter.scala
@@ -7,15 +7,14 @@ object Day1 extends App{
   val source = args.headOption.fold(Source.stdin)(Source.fromFile)
 
   val calories = source.getLines
-    .foldLeft(List.empty[List[Int]]) {
-      case (acc, "")         => Nil :: acc
-      case (head :: tail, x) => (x.toInt :: head) :: tail
-      case (Nil, x)          => (x.toInt :: Nil) :: Nil
+    .foldLeft(List.empty[Int]) {
+      case (cals       , "")   => 0 :: cals
+      case (sum :: cals, line) => (line.toInt + sum) :: cals
+      case (Nil        , line) => line.toInt :: Nil
     }
-    .map(_.sum)
     .sorted(Ordering[Int].reverse)
 
-  println(s"Part 1: ${calories.headOption.getOrElse(0)}")
+  println(s"Part 1: ${calories.head}")
   println(s"Part 2: ${calories.take(3).sum}")
 
 }
